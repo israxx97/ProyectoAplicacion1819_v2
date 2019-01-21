7ad7dea66700aad6d4372e8ae59b9e6925beaab9
israxx97 <isragarcia97@gmail.com>
2019-01-21 13:50:55 +0100

    Create configDB.php

---
+++ config/configDB.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define('HOST_DB', 'mysql:host=localhost;dbname=DAW202_ProyectoAplicacion1819');
+define('PASSWORD_DB', 'paso');
+define('USER_DB', 'user202ProyectoAplicacion1819');
