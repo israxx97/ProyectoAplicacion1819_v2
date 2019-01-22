7e50e07394f35896b1efddd8ecf11b9f5d81b7ba
israel garcia <israel.garcab@educa.jcyl.es>
2019-01-22 10:13:36 +0100

    + v0.4.1

---
+++ config/configDB.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+define('HOST_DB', 'mysql:host=192.168.20.19;dbname=DAW202_ProyectoAplicacion1819');
+define('PASSWORD_DB', 'paso');
+define('USER_DB', 'user202ProyectoAplicacion1819');
