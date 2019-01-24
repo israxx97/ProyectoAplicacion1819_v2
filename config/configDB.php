082de3be15be132c60bd51957cf79e8d3ba28692
israxx97 <isragarcia97@gmail.com>
2019-01-24 18:10:32 +0100

    + v0.6 + Cambios en layout.php y vLogin.php. + Corregido error al no encontrar los estilos en la pantalla de login.

--- config/configDB.php
+++ config/configDB.php
@@ -5,6 +5,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-define('HOST_DB', 'mysql:host=192.168.20.19;dbname=DAW202_ProyectoAplicacion1819');
+define('HOST_DB', 'mysql:host=localhost;dbname=DAW202_ProyectoAplicacion1819');
 define('PASSWORD_DB', 'paso');
 define('USER_DB', 'user202ProyectoAplicacion1819');
